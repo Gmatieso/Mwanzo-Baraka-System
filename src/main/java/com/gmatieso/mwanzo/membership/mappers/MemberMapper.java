eeb8e8870165650a4f008c15acb9bd57725623ff
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2025-06-03 23:59:19 +0300

    Feat: Modeling our MemberMapper class

--- src/main/java/com/gmatieso/mwanzo/membership/mappers/MemberMapper.java
+++ src/main/java/com/gmatieso/mwanzo/membership/mappers/MemberMapper.java
@@ -1,4 +1,19 @@
 package com.gmatieso.mwanzo.membership.mappers;
 
+import com.gmatieso.mwanzo.membership.dtos.MemberResponse;
+import com.gmatieso.mwanzo.membership.entity.Member;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface MemberMapper {
+    Member toEntity(MemberResponse memberResponse);
+
+
+    @Mapping(source = "contribution.id", target = "contributionId")
+    @Mapping(source = "share.id", target = "shareId")
+    @Mapping(source = "user.id", target = "userId")
+    @Mapping(source = "loan.id", target = "loanId")
+    @Mapping(source = "guarantor.id", target = "guarantorId")
+    MemberResponse toResponse(Member member);
 }
