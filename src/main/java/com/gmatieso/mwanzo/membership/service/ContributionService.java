704f37b82abcdd4851a7801bc8c3395614780425
Gmatieso <matiesogeoffrey@gmail.com>
2025-06-03 16:37:31 +0300

    Feat: Model our ContributionService Interface done

--- src/main/java/com/gmatieso/mwanzo/membership/service/ContributionService.java
+++ src/main/java/com/gmatieso/mwanzo/membership/service/ContributionService.java
@@ -1,4 +1,17 @@
 package com.gmatieso.mwanzo.membership.service;
 
+import com.gmatieso.mwanzo.membership.dtos.ContributionRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
 public interface ContributionService {
+   ResponseEntity<?> getAllContribution(Pageable pageable);
+
+   ResponseEntity<?> getContributionById(Long id);
+
+   ResponseEntity<?> createContribution(ContributionRequest request);
+
+   ResponseEntity<?> updateContribution(Long id, ContributionRequest request);
+
+   ResponseEntity<?> deleteContribution(Long id);
 }
