f22bd27ef8787e65ae177886b03be787c8787e94
Gmatieso <matiesogeoffrey@gmail.com>
2025-06-04 16:00:44 +0300

    Feat: Define helper utility class to standardize how HTTP success responses are returned

---
+++ src/main/java/com/gmatieso/mwanzo/common/ApiResponseEntity.java
@@ -0,0 +1,29 @@
+package com.gmatieso.mwanzo.common;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class ApiResponseEntity {
+   public static ResponseEntity<?> success(String message) {
+       ApiResponse<?> response = new ApiResponse<>(HttpStatus.OK.value(), message);
+       return ResponseEntity.ok(response);
+    }
+
+    public static <T> ResponseEntity<?> success(String message, T data) {
+       if(data instanceof Page<?>) {
+           Page<T> page = (Page<T>) data;
+           PaginatedResponse<?> paginatedResponse = new PaginatedResponse<>(
+                   page.getContent(),
+                   page.getNumber() + 1,
+                   page.getSize(),
+                   page.getTotalElements(),
+                   page.getTotalPages(),
+                   page.isLast()
+           );
+           return ResponseEntity.ok(new ApiResponse<>(HttpStatus.OK.value(), message,paginatedResponse));
+       }
+       return  ResponseEntity.ok(new ApiResponse<>(HttpStatus.OK.value(), message, data));
+
+    }
+}
---
+++ src/main/java/com/gmatieso/mwanzo/common/PaginatedResponse.java
@@ -0,0 +1,12 @@
+package com.gmatieso.mwanzo.common;
+
+import java.util.List;
+
+public record PaginatedResponse<T>(List<T> content,
+                                   int pageNumber,
+                                   int size,
+                                   long totalElements,
+                                   int totalPages,
+                                   boolean isLastPage
+                                   ) {
+}
