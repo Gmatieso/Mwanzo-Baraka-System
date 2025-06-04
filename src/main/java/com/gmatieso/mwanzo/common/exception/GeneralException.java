209cec0f6b8df556c9202a96956fd9be95c01406
Gmatieso <matiesogeoffrey@gmail.com>
2025-06-04 17:48:00 +0300

    Feat: Modeling our exceptional handler class

---
+++ src/main/java/com/gmatieso/mwanzo/common/exception/GeneralException.java
@@ -0,0 +1,7 @@
+package com.gmatieso.mwanzo.common.exception;
+
+public class GeneralException extends RuntimeException {
+  public GeneralException(String message) {
+    super(message);
+  }
+}
--- src/main/java/com/gmatieso/mwanzo/common/exception/ResourceNotFoundException.java
+++ src/main/java/com/gmatieso/mwanzo/common/exception/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package com.gmatieso.mwanzo.common.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+  public ResourceNotFoundException(String message) {
+    super(message);
+  }
 }
