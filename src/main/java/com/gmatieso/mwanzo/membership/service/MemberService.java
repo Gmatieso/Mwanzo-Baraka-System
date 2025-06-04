b3ea89c43ae8a0b27515aa9584bb743813227279
Gmatieso <matiesogeoffrey@gmail.com>
2025-06-04 14:42:50 +0300

    Feat: Modelling our MemberService and MemberServiceImpl done

--- src/main/java/com/gmatieso/mwanzo/membership/service/MemberService.java
+++ src/main/java/com/gmatieso/mwanzo/membership/service/MemberService.java
@@ -1,4 +1,20 @@
 package com.gmatieso.mwanzo.membership.service;
 
+import com.gmatieso.mwanzo.membership.dtos.MemberRequest;
+import com.gmatieso.mwanzo.membership.entity.Member;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
 public interface MemberService {
+    ResponseEntity<?> createMember(MemberRequest memberRequest);
+
+    ResponseEntity<?> updateMember(Long id, MemberRequest memberRequest);
+
+    ResponseEntity<?> deleteMember(Long id);
+
+    ResponseEntity<?> getMember(Long id);
+
+    ResponseEntity<?> getMembers(Pageable pageable);
+
+    Member getMemberByIdOrThrow(Long id);
 }
--- src/main/java/com/gmatieso/mwanzo/membership/service/MemberServiceImpl.java
+++ src/main/java/com/gmatieso/mwanzo/membership/service/MemberServiceImpl.java
@@ -1,4 +1,38 @@
 package com.gmatieso.mwanzo.membership.service;
 
-public class MemberServiceImpl {
+import com.gmatieso.mwanzo.membership.dtos.MemberRequest;
+import com.gmatieso.mwanzo.membership.entity.Member;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+public class MemberServiceImpl implements MemberService {
+    @Override
+    public ResponseEntity<?> createMember(MemberRequest memberRequest) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<?> updateMember(Long id, MemberRequest memberRequest) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<?> deleteMember(Long id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<?> getMember(Long id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<?> getMembers(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public Member getMemberByIdOrThrow(Long id) {
+        return null;
+    }
 }
