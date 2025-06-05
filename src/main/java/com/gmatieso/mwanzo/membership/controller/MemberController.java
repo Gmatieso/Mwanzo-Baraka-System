52a481869e366cef1f808a58a64b4bda94866f6e
Gmatieso <matiesogeoffrey@gmail.com>
2025-06-05 18:39:39 +0300

    Feat: Implementation of createMember Method done

--- src/main/java/com/gmatieso/mwanzo/membership/controller/MemberController.java
+++ src/main/java/com/gmatieso/mwanzo/membership/controller/MemberController.java
@@ -1,10 +1,27 @@
 package com.gmatieso.mwanzo.membership.controller;
 
+import com.gmatieso.mwanzo.common.config.ApiConfig;
+import com.gmatieso.mwanzo.membership.dtos.MemberRequest;
+import com.gmatieso.mwanzo.membership.service.MemberService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping()
-public class Member {
-    
+@RestController()
+@RequestMapping(MemberController.PATH)
+public class MemberController {
+    public static final String PATH = ApiConfig.BASE_API_PATH + "membership";
+
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createMember(MemberRequest memberRequest) {
+        return memberService.createMember(memberRequest);
+
+    }
 }
