400f52db07139bc5ffc9ba23816e0c44144ac978
Gmatieso <matiesogeoffrey@gmail.com>
2025-06-05 18:33:05 +0300

    Fix: Implement a custome mapMember method that maps a member to a Member Response

--- src/main/java/com/gmatieso/mwanzo/membership/mappers/ContributionMapper.java
+++ src/main/java/com/gmatieso/mwanzo/membership/mappers/ContributionMapper.java
@@ -1,15 +1,32 @@
 package com.gmatieso.mwanzo.membership.mappers;
 
 import com.gmatieso.mwanzo.membership.dtos.ContributionResponse;
+import com.gmatieso.mwanzo.membership.dtos.MemberResponse;
 import com.gmatieso.mwanzo.membership.entity.Contribution;
+import com.gmatieso.mwanzo.membership.entity.Member;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ContributionMapper {
 
    Contribution toEntity(ContributionResponse contributionresponse);
 
-    @Mapping(source = "member.id", target = "members")
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "contributionDate", target = "contributionDate")
+    @Mapping(source = "amount", target = "amount")
+    @Mapping(source = "groupShareAmount", target = "groupShareAmount")
+    @Mapping(source = "individualShareAmount", target = "individualShareAmount")
+    @Mapping(source = "member", target = "members")
     ContributionResponse toResponse(Contribution contribution);
+
+
+    @Named("mapMember")
+    default MemberResponse mapMember(Member member) {
+        return member != null ? new MemberResponse(member.getId(), member.getName(), null, null): null;
+    }
+
 }
