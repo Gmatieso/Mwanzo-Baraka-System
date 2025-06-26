91b0d470718e95db47fc59ef01c2035347a76902
Gmatieso <matiesogeoffrey@gmail.com>
2025-06-26 16:18:22 +0300

    Feat: Create a GroupMember request record

--- src/main/java/com/gmatieso/mwanzo/membership/dtos/GroupMemberRequest.java
+++ src/main/java/com/gmatieso/mwanzo/membership/dtos/GroupMemberRequest.java
@@ -1,4 +1,8 @@
 package com.gmatieso.mwanzo.membership.dtos;
 
-public record GroupMemberRequest() {
+import jakarta.validation.constraints.NotEmpty;
+
+public record GroupMemberRequest(
+       @NotEmpty(message = "Group Member name is required") String name
+) {
 }
