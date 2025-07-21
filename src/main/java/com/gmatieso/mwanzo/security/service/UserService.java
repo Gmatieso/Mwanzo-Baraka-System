e089cf48cbb4be906578afd31f74ccf95b19ccee
Gmatieso <matiesogeoffrey@gmail.com>
2025-07-21 22:12:53 +0300

    Fix: Modeling our UserService Layer done

--- src/main/java/com/gmatieso/mwanzo/security/service/UserService.java
+++ src/main/java/com/gmatieso/mwanzo/security/service/UserService.java
@@ -1,4 +1,18 @@
 package com.gmatieso.mwanzo.security.service;
 
+import com.gmatieso.mwanzo.security.dtos.UserRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
 public interface UserService {
+    ResponseEntity<?> createUser(UserRequest userRequest);
+
+    ResponseEntity<?> updateUser(Long id, UserRequest userRequest);
+
+    ResponseEntity<?> deleteUser(Long id);
+
+    ResponseEntity<?> getUsers(Pageable pageable);
+
+    ResponseEntity<?> getUser(Long id);
+
 }
