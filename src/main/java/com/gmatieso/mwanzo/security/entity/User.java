4c07be682c058d327825ff3ec260224ca6a56ff8
Gmatieso <matiesogeoffrey@gmail.com>
2025-07-21 16:06:50 +0300

    Feat: One User Many membership i.e Individual or Group

--- src/main/java/com/gmatieso/mwanzo/security/entity/User.java
+++ src/main/java/com/gmatieso/mwanzo/security/entity/User.java
@@ -3,28 +3,65 @@ package com.gmatieso.mwanzo.security.entity;
 import com.gmatieso.mwanzo.common.utils.Role;
 import com.gmatieso.mwanzo.membership.entity.Member;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
-@Table(name = "users")
+@Table(
+        name = "users",
+        indexes = {
+                @Index(name = "idx_user_email", columnList = "email"),
+                @Index(name = "idx_user_phone", columnList = "phone"),
+                @Index(name = "idx_user_username", columnList = "username")
+        }
+ )
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id", nullable = false)
     private Long id;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @NotBlank(message = "First name is required")
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+
+    @NotBlank(message = "Last name is required")
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @NotBlank(message = "Phone number is required")
+    @Size(min = 10, max = 15, message = "Phone number Must be between 10 and 15 digits")
+    @Pattern(
+            regexp = "^[0-9+\\-() ]+$",
+            message = "Phone number can only contain digits, +, -, spaces, or parentheses"
+    )
+    private String phone;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
     @JoinColumn(name = "member_id")
-    private Member member;
+    private List<Member> member = new ArrayList<>();
 
-
-    @Column(name = "username")
+    @NotBlank(message = "Username is required")
+    @Column(name = "username", nullable = false, unique = true)
     private String username;
 
-    @Column(name = "password")
+    @NotBlank(message = "Password is required")
+    @Column(name = "password", nullable = false)
     private String password;
 
     @Column(name = "role")
