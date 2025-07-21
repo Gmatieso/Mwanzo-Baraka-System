677ef8acb308d32e30866995e405d3d7202bc278
Gmatieso <matiesogeoffrey@gmail.com>
2025-07-21 17:54:35 +0300

    Fix: Defining our Mappers

--- src/main/java/com/gmatieso/mwanzo/security/mappers/UserMapper.java
+++ src/main/java/com/gmatieso/mwanzo/security/mappers/UserMapper.java
@@ -1,4 +1,18 @@
 package com.gmatieso.mwanzo.security.mappers;
 
+import com.gmatieso.mwanzo.security.dtos.UserResponse;
+import com.gmatieso.mwanzo.security.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface UserMapper {
+    User toEntity(UserResponse userResponse);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "firstName", target = "firstName")
+    @Mapping(source = "lastName", target = "lastName")
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "phone", target = "phone")
+    UserResponse toResponse(User user);
 }
