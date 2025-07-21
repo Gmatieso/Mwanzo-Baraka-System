9ef6a9d1b4f6e35c5ff11b1dffbe97fd3a009e53
Gmatieso <matiesogeoffrey@gmail.com>
2025-07-22 02:26:48 +0300

    Fix: Setting our user controller done

---
+++ src/main/java/com/gmatieso/mwanzo/security/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.gmatieso.mwanzo.security.controller;
+
+import com.gmatieso.mwanzo.common.config.ApiConfig;
+import com.gmatieso.mwanzo.security.dtos.UserRequest;
+import com.gmatieso.mwanzo.security.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping(UserController.PATH)
+public class UserController {
+    public static final String PATH = ApiConfig.BASE_API_PATH + "users";
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createUser(@RequestBody UserRequest userRequest){
+        return userService.createUser(userRequest);
+    }
+}
