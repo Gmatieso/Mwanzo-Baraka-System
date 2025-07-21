dff6e9657a286599bbd33776277f5eb922d81b45
Gmatieso <matiesogeoffrey@gmail.com>
2025-07-21 20:45:07 +0300

    Fix: Setting our UserResponse and UserMapper

--- src/main/java/com/gmatieso/mwanzo/security/dtos/UserResponse.java
+++ src/main/java/com/gmatieso/mwanzo/security/dtos/UserResponse.java
@@ -1,9 +1,15 @@
 package com.gmatieso.mwanzo.security.dtos;
 
+import com.gmatieso.mwanzo.membership.dtos.MemberResponse;
+
+import java.util.List;
+
 public record UserResponse(
         Long id,
         String firstName,
         String lastName,
         String email,
-        String phone) {
+        String phone,
+        List<MemberResponse> members
+        ) {
 }
--- src/main/java/com/gmatieso/mwanzo/security/mappers/UserMapper.java
+++ src/main/java/com/gmatieso/mwanzo/security/mappers/UserMapper.java
@@ -1,18 +1,40 @@
 package com.gmatieso.mwanzo.security.mappers;
 
+import com.gmatieso.mwanzo.membership.dtos.MemberResponseBasic;
+import com.gmatieso.mwanzo.membership.entity.Member;
 import com.gmatieso.mwanzo.security.dtos.UserResponse;
 import com.gmatieso.mwanzo.security.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-    User toEntity(UserResponse userResponse);
+    User toEntity(UserResponse userResponseBasic);
 
     @Mapping(source = "id", target = "id")
     @Mapping(source = "firstName", target = "firstName")
     @Mapping(source = "lastName", target = "lastName")
     @Mapping(source = "email", target = "email")
     @Mapping(source = "phone", target = "phone")
+    @Mapping(source = "members", target = "members", qualifiedByName = "mapMembers")
     UserResponse toResponse(User user);
+
+    @Named("mapMembers")
+    default List<MemberResponseBasic> mapMembers(List<Member> members){
+        return members != null ? members.stream()
+                .map(member -> new MemberResponseBasic(
+                        member.getId(),
+                        member.getName(),
+                        member.getMemberType(),
+                        member.getRegistrationFees(),
+                        member.getStatus()
+                ))
+                .collect(Collectors.toList()) : Collections.emptyList();
+
+    }
 }
