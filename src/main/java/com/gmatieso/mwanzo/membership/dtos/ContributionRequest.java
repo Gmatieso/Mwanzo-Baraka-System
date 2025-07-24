e9431c15139a4ce59f33be7cfb39cd33d8964040
Gmatieso <matiesogeoffrey@gmail.com>
2025-07-24 23:09:54 +0300

    Fix: Refactor not Empty to use not Null for numeric fields

--- src/main/java/com/gmatieso/mwanzo/membership/dtos/ContributionRequest.java
+++ src/main/java/com/gmatieso/mwanzo/membership/dtos/ContributionRequest.java
@@ -2,15 +2,20 @@ package com.gmatieso.mwanzo.membership.dtos;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import jakarta.validation.constraints.NotEmpty;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 
 public record ContributionRequest(
         @NotEmpty String memberId,
-        @NotEmpty BigDecimal amount,
+        @NotNull(message = "Amount is required")
+        @DecimalMin(value = "0.0", inclusive = false, message = "Amount must be greater than zero")
+        BigDecimal amount,
         LocalDateTime contributionDate,
         BigDecimal groupShareAmount,
         BigDecimal individualShareAmount
 ) {
+
 }
