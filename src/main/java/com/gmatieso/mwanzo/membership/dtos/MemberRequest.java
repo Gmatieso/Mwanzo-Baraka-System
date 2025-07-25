fd65edb1280672765ed91aea77998b979a5865fb
Gmatieso <matiesogeoffrey@gmail.com>
2025-07-25 11:37:18 +0300

    Fix: validating user_id to ensure it's not null and represents a valid user reference

--- src/main/java/com/gmatieso/mwanzo/membership/dtos/MemberRequest.java
+++ src/main/java/com/gmatieso/mwanzo/membership/dtos/MemberRequest.java
@@ -2,13 +2,14 @@ package com.gmatieso.mwanzo.membership.dtos;
 
 import com.gmatieso.mwanzo.common.utils.MemberType;
 import com.gmatieso.mwanzo.security.entity.User;
+import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public record MemberRequest(
-
+        @NotNull(message = "User ID is required")
         Long user_id,
 //        List<Long> contributionId,
         BigDecimal registrationFees,
