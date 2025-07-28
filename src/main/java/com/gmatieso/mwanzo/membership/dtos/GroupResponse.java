ce02c2d08d69dc3c1720d0c64b5f6c2bc7a292c9
Gmatieso <matiesogeoffrey@gmail.com>
2025-07-28 15:38:20 +0300

    Fix: Modelling our GroupResponse and GroupResponseBasic done

--- src/main/java/com/gmatieso/mwanzo/membership/dtos/GroupResponse.java
+++ src/main/java/com/gmatieso/mwanzo/membership/dtos/GroupResponse.java
@@ -1,4 +1,18 @@
 package com.gmatieso.mwanzo.membership.dtos;
 
-public record GroupResponse() {
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record GroupResponse(
+    Long id,
+    String name,
+    String registrationNumber,
+    String email,
+    String location,
+    LocalDateTime dateRegistered,
+    String postalAddress,
+    String groupCategory,
+    String sector,
+    List<MemberResponse> members
+) {
 }
--- src/main/java/com/gmatieso/mwanzo/membership/dtos/GroupResponseBasic.java
+++ src/main/java/com/gmatieso/mwanzo/membership/dtos/GroupResponseBasic.java
@@ -1,4 +1,15 @@
 package com.gmatieso.mwanzo.membership.dtos;
 
-public record GroupResponseBasic() {
+import java.time.LocalDateTime;
+
+public record GroupResponseBasic(
+        Long id,
+        String registrationNumber,
+        String email,
+        String location,
+        LocalDateTime dateRegistered,
+        String postalAddress,
+        String groupCategory,
+        String sector
+) {
 }
