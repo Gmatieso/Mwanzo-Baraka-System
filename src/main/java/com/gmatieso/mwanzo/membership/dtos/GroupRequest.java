bbe100ca3293c53f8a3b17e044740ea124add169
Gmatieso <matiesogeoffrey@gmail.com>
2025-07-28 14:46:48 +0300

    FIX: Modeling our GroupRequest

--- src/main/java/com/gmatieso/mwanzo/membership/dtos/GroupRequest.java
+++ src/main/java/com/gmatieso/mwanzo/membership/dtos/GroupRequest.java
@@ -1,4 +1,17 @@
 package com.gmatieso.mwanzo.membership.dtos;
 
-public record GroupRequest() {
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record GroupRequest(
+        String name,
+        String registrationNumber,
+        String email,
+        String location,
+        LocalDateTime dateRegistered,
+        String postalAddress,
+        String groupCategory,
+        String sector,
+        List<Long> membersId
+) {
 }
