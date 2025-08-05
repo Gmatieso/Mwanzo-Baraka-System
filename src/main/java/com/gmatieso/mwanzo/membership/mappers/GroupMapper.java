6376bcb81a21bfedb877b6f4339b282f4db55317
Gmatieso <matiesogeoffrey@gmail.com>
2025-08-05 14:08:16 +0300

    Fix: Implementing our Mappers

--- src/main/java/com/gmatieso/mwanzo/membership/mappers/GroupMapper.java
+++ src/main/java/com/gmatieso/mwanzo/membership/mappers/GroupMapper.java
@@ -1,4 +1,51 @@
 package com.gmatieso.mwanzo.membership.mappers;
 
+import com.gmatieso.mwanzo.membership.dtos.*;
+import com.gmatieso.mwanzo.membership.entity.Group;
+import com.gmatieso.mwanzo.membership.entity.Member;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public interface GroupMapper {
+    Group toEntity(GroupRequest groupRequest);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "registrationNumber", target = "registrationNumber")
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "location", target = "location")
+    @Mapping(source = "dateRegistered", target = "dateRegistered")
+    @Mapping(source = "postalAddress", target = "postalAddress")
+    @Mapping(source = "groupCategory", target = "groupCategory")
+    @Mapping(source = "sector", target = "sector")
+    GroupResponse toResponse(Group group);
+
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "name", target = "name")
+    @Mapping(source = "registrationNumber", target = "registrationNumber")
+    @Mapping(source = "email", target = "email")
+    @Mapping(source = "location", target = "location")
+    @Mapping(source = "dateRegistered", target = "dateRegistered")
+    @Mapping(source = "postalAddress", target = "postalAddress")
+    @Mapping(source = "groupCategory", target = "groupCategory")
+    @Mapping(source = "sector", target = "sector")
+    @Mapping(source = "members", target = "members", qualifiedByName = "mapMembers")
+    GroupResponseBasic toResponseBasic(Group group);
+
+    @Named("mapMembers")
+    default List<MemberResponseBasic> mapGroupMember(List<Member> memberList){
+            return memberList != null ? memberList.stream()
+                    .map(member -> new MemberResponseBasic(
+                            member.getId(),
+                            member.getMemberType(),
+                            null,
+                            member.getRegistrationFees(),
+                            member.getStatus()
+                    ))
+                    .collect(Collectors.toList()) : Collections.emptyList();
+    }
 }
