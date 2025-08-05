598af8618efd4da401e20f3241968536120b7583
Gmatieso <matiesogeoffrey@gmail.com>
2025-08-05 10:50:54 +0300

    Fix: Setting Getter and Setter for our User entity

--- src/main/java/com/gmatieso/mwanzo/security/entity/User.java
+++ src/main/java/com/gmatieso/mwanzo/security/entity/User.java
@@ -55,4 +55,61 @@ public class User {
 
     @Column(name = "role")
     private Role role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
