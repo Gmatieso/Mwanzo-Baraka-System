6a99db78478454631864c0f04cc730d5cf3d6a74
Gmatieso <matiesogeoffrey@gmail.com>
2025-08-05 23:12:57 +0300

    Refactor: Our Group Response basic

--- src/main/java/com/gmatieso/mwanzo/membership/dtos/GroupResponseBasic.java
+++ src/main/java/com/gmatieso/mwanzo/membership/dtos/GroupResponseBasic.java
@@ -6,12 +6,13 @@ import java.util.List;
 public record GroupResponseBasic(
         Long id,
         String registrationNumber,
+        String name,
         String email,
         String location,
         LocalDateTime dateRegistered,
         String postalAddress,
         String groupCategory,
         String sector,
-        List<MemberResponseBasic> member
+        List<MemberResponseBasic> members
 ) {
 }
