2c6b1f569aa362467ce384dbc844aaf6aa0785f1
Gmatieso <matiesogeoffrey@gmail.com>
2025-08-06 10:44:42 +0300

    Fix: Refactor Group name to groups

--- src/main/resources/db/migration/V014__Add__Group_Reg_Fees_And_Phone.sql
+++ src/main/resources/db/migration/V014__Add__Group_Reg_Fees_And_Phone.sql
@@ -1,8 +1,8 @@
 -- Add registration_fee column to group table
 
-ALTER TABLE "Groups"
+ALTER TABLE "groups"
 ADD COLUMN registration_fee DECIMAL NOT NULL;
 
 -- Add phone column to group table
-ALTER TABLE "Groups"
+ALTER TABLE "groups"
 ADD COLUMN phone VARCHAR(20);
