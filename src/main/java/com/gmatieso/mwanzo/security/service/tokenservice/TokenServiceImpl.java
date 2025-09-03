8fcdca7560bd00da01ba855d4e89b654ca4229e8
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-03 12:31:46 +0300

    Chore: Implement our TokenServiceImpl

--- src/main/java/com/gmatieso/mwanzo/security/service/tokenservice/TokenServiceImpl.java
+++ src/main/java/com/gmatieso/mwanzo/security/service/tokenservice/TokenServiceImpl.java
@@ -1,30 +1,63 @@
-package com.gmatieso.mwanzo.security.service;
+package com.gmatieso.mwanzo.security.service.tokenservice;
 
+import com.gmatieso.mwanzo.common.utils.TokenType;
+import com.gmatieso.mwanzo.security.entity.Token;
 import com.gmatieso.mwanzo.security.entity.User;
+import com.gmatieso.mwanzo.security.repository.TokenRepository;
+import lombok.RequiredArgsConstructor;
 
-public class TokenServiceImpl implements TokenService{
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class TokenServiceImpl implements TokenService {
+
+    private final TokenRepository tokenRepository;
+
     @Override
     public void saveNewToken(String token, User user) {
-        Token newToken = new Token()
+        Token newToken = new Token();
+        newToken.setToken(token);
+        newToken.setUser(user);
+        newToken.setTokenType(TokenType.BEARER);
+
+        tokenRepository.save(newToken);
+
     }
 
     @Override
-    public boolean isValidToken(String token) {
-        return false;
+    public boolean isValidToken(String jwtToken) {
+        return tokenRepository.findByToken(jwtToken)
+                .filter(token -> !token.isExpired() && !token.isInvalid())
+                .isPresent();
     }
 
     @Override
-    public void invalidateToken(String token) {
-
+    public void invalidateToken(String jwtToken) {
+        Token token = tokenRepository.findByToken(jwtToken).orElse(null);
+        if (token != null) {
+            token.setExpired(true);
+            token.setInvalid(true);
+            tokenRepository.save(token);
+        }
     }
 
     @Override
     public void revokeUserTokens(User user) {
-
+        List<Token> validTokens = tokenRepository.findAllValidTokensByUser(String.valueOf(user.getId()));
+        if (validTokens.isEmpty()) {
+            return;
+        }
+        validTokens.forEach(t -> {
+            t.setInvalid(true);
+            t.setExpired(true);
+        });
+        tokenRepository.saveAll(validTokens);
     }
 
     @Override
     public boolean isTokenPresent(String token) {
-        return false;
+        Optional<Token> tokenOptional =  tokenRepository.findByToken(token);
+        return tokenOptional.isPresent();
     }
 }
