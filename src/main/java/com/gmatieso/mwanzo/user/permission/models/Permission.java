7eeb05ee4e9b04e2a73708bb0d18a8f82cc09d60
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-04 23:10:37 +0300

    Chore: Modeling our entity done

--- src/main/java/com/gmatieso/mwanzo/user/permission/models/Permission.java
+++ src/main/java/com/gmatieso/mwanzo/user/permission/models/Permission.java
@@ -1,4 +1,41 @@
 package com.gmatieso.mwanzo.user.permission.models;
 
+
+import com.gmatieso.mwanzo.user.permission_group.models.PermissionGroup;
+import com.gmatieso.mwanzo.user.role.models.Role;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "permissions")
+@Data
 public class Permission {
+    @Id
+    @UuidGenerator
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "permission_group_id", nullable = false)
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private PermissionGroup permissionGroup;
+
+    @ManyToMany(mappedBy = "permissions")
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private List<Role> roles;
+
+
+
+
+
 }
---
+++ src/main/java/com/gmatieso/mwanzo/user/permission_group/models/PermissionGroup.java
@@ -0,0 +1,4 @@
+package com.gmatieso.mwanzo.user.permission_group.models;
+
+public class PermissionGroup {
+}
