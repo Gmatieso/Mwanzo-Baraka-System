8f211eec59cf0f51d7b43edbf70c17f81868c065
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-05 14:50:19 +0300

    Chore: Modelling our permissionInitializer

---
+++ src/main/java/com/gmatieso/mwanzo/user/permission_group/configs/PermissionInitializer.java
@@ -0,0 +1,12 @@
+package com.gmatieso.mwanzo.user.permission_group.configs;
+
+import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@Component
+public class PermissionInitializer {
+    private static  final Logger logger = LoggerFactory.getLogger(PermissionInitializer.class);
+
+}
