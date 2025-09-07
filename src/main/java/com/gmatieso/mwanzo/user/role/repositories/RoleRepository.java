b7a1cfa393be6876bd27dd8fd34ec8892f688bc5
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-07 22:06:59 +0300

    Chore: Modelling our repository done

--- src/main/java/com/gmatieso/mwanzo/user/role/repositories/RoleRepository.java
+++ src/main/java/com/gmatieso/mwanzo/user/role/repositories/RoleRepository.java
@@ -4,6 +4,7 @@ import com.gmatieso.mwanzo.user.role.enums.RoleEnum;
 import com.gmatieso.mwanzo.user.role.models.Role;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -11,9 +12,10 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.UUID;
 
 @Repository
-public interface RoleRepository {
+public interface RoleRepository extends JpaRepository<Role, UUID> {
     boolean existsByName(RoleEnum name);
 
     @Query("SELECT r FROM Role r WHERE (:name IS NULL OR r.name = :name)")
