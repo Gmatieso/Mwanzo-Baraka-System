d25eedbc055dd9e3ef5e4f44686639eaf8b3e6e9
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-08 17:57:22 +0300

    Feat: Modeling our AuthService done

--- src/main/java/com/gmatieso/mwanzo/user/user/auth/services/AuthService.java
+++ src/main/java/com/gmatieso/mwanzo/user/user/auth/services/AuthService.java
@@ -1,4 +1,22 @@
 package com.gmatieso.mwanzo.user.user.auth.services;
 
+import com.gmatieso.mwanzo.user.user.auth.dtos.AdminSignInRequest;
+import com.gmatieso.mwanzo.user.user.auth.dtos.AuthTokenDto;
+import com.gmatieso.mwanzo.user.user.auth.dtos.LoginRequest;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+
+import java.io.IOException;
+
 public interface AuthService {
+
+   ResponseEntity<?> login(@Valid LoginRequest loginRequest);
+
+   ResponseEntity<?> checkPhone(String phone);
+
+   AuthTokenDto signinAdmin(AdminSignInRequest adminSignInRequest);
+
+   void refreshToken(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException;
 }
