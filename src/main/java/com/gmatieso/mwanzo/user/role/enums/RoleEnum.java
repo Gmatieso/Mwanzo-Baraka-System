9eb90edd79a2e9c6f5fabbb865d3b21dff90ce06
Gmatieso <matiesogeoffrey@gmail.com>
2025-09-08 15:31:19 +0300

    Feat: Implement getInternalRoles and getRolesByName methods done

--- src/main/java/com/gmatieso/mwanzo/user/role/enums/RoleEnum.java
+++ src/main/java/com/gmatieso/mwanzo/user/role/enums/RoleEnum.java
@@ -1,13 +1,38 @@
 package com.gmatieso.mwanzo.user.role.enums;
 
+import com.gmatieso.mwanzo.common.exception.BadRequestException;
+
+import java.util.Arrays;
+import java.util.List;
+
 public enum RoleEnum {
-    SUPER_ADMIN(),
-    ADMIN(),
-    TREASURER(),
-    AUDITOR(),
-    LOAN_OFFICER(),
-    CREDIT_COMMITTEE(),
-    INDIVIDUAL_MEMBER(),
-    GROUP_REPRESENTATIVE(),
-    GROUP_MEMBER()
+    SUPER_ADMIN(true),
+    ADMIN(true),
+    TREASURER(true),
+    AUDITOR(true),
+    LOAN_OFFICER(true),
+    CREDIT_COMMITTEE(true),
+    INDIVIDUAL_MEMBER(true),
+    GROUP_REPRESENTATIVE(true),
+    GROUP_MEMBER(true);
+
+    public final boolean isInternal;
+
+    RoleEnum(boolean isInternal) {
+        this.isInternal = isInternal;
+    }
+
+    public static List<RoleEnum> getInternalRoles() {
+        return Arrays.stream(RoleEnum.values())
+                .filter(role -> role.isInternal)
+                .toList();
+    }
+
+    public static RoleEnum getRoleByName(String roleName) {
+        try{
+            return RoleEnum.valueOf(roleName);
+        }catch (IllegalArgumentException e) {
+            throw new BadRequestException("Invalid role group: " + roleName);
+        }
+    }
 }
